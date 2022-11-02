b65d0a4745b79d9190517647214013c50147c188
Santiago Correa Aguirre <santiago.c.a_10@hotmail.es>
2022-11-02 15:46:38 -0500

    Configuración básica dle entorno de desarrollo de la aplicación.

---
+++ src/assets/scripts/router.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+// Importar layouts
+import MainLayout from '../../layouts/MainLayout.vue'
+
+// Importar páginas
+import Index from '../../pages/Index.vue'
+
+import NotFound from '../../pages/NotFound.vue'
+
+const routes = [
+	{
+		path: '/',
+		component: MainLayout,
+		children: [
+			{
+				path: '/',
+				component: Index
+			},
+		]
+	},
+	{
+		path: '/:pathMatch(.*)*',
+		name: 'not-found',
+		component: NotFound
+	},
+	{
+		path: '/:pathMatch(.*)',
+		name: 'bad-not-found',
+		component: NotFound
+	},
+]
+
+const router = createRouter({
+	routes,
+	history: createWebHistory()
+})
+
+router.resolve({
+	name: 'not-found',
+	params: { pathMatch: ['not', 'found'] },
+}).href 
+
+export default router;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/es'
+import router from './assets/scripts/router.js'
+
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+
+// Import styles
+import 'quasar/src/css/index.sass'
 import './assets/styles/style.css'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(Quasar, {
+	plugins: {},
+	lang: quasarLang
+})
+.use(router)
+.mount('#app')
